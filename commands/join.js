79fb87e48d11b9fd2b35602413e7edff420aba9f
Alex <avhoi83@gmail.com>
2022-07-05 18:35:29 -0700

    fixed formatting errors

--- commands/join.js
+++ commands/join.js
@@ -1,25 +1,24 @@
 module.exports = {
-    name: 'join',
-    description: 'Join a game.',
-    aliases: ['j'],
-    shortUsage(prefix) {
-        return `\`${prefix}j\``
-    },
-    longUsage(prefix) {
-        return `\`${prefix}join\``
-    },
-    category: 'Main',
-    // category: 'Paid',
-    permsAllowed: ['VIEW_CHANNEL'],
-    usersAllowed: ['217385992837922819'],
-    execute: async function (message) {
-        const returnMsg = []
-        try {
-
-        } catch (error) {
-            throw error
-        }
-
-        return returnMsg
+  name: 'join',
+  description: 'Join a game.',
+  aliases: ['j'],
+  shortUsage(prefix) {
+    return `\`${prefix}j\``;
+  },
+  longUsage(prefix) {
+    return `\`${prefix}join\``;
+  },
+  category: 'Main',
+  // category: 'Paid',
+  permsAllowed: ['VIEW_CHANNEL'],
+  usersAllowed: ['217385992837922819'],
+  execute: async function (message) {
+    const returnMsg = [];
+    try {
+    } catch (error) {
+      throw error;
     }
+
+    return returnMsg;
+  },
 };
